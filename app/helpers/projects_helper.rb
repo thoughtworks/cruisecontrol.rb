457fad5921d46a9b7fe7debce0a0888e03f2b9fc
Arthur Tam <arthur.tam@gmail.com>
2007-01-27 00:08:25 +0000

    r63: 1) Display tests that failed during the build process 2) Displaying basic information for tests that failed (message and stack trace)

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,6 +6,24 @@ def format_build_log(log)
   convert_new_lines(log.gsub(/(\d+ tests, \d+ assertions, \d+ failures, \d+ errors)/, '<div class="test-results">\1</div>'))
 end
 
+def display_test_failures_if_any(log)
+  output = String.new
+
+  testFailures = TestFailureParser.new.get_test_failures(log)
+  testFailures.each {|testFailure| output << format_test_failure_output(testFailure)} 
+
+  if output != String.new
+    output.strip
+  else
+    "None"
+  end
+end
+
+def format_test_failure_output(testFailure)
+  "Message: " + testFailure.message.gsub(/\\n/, "\n") + "\n\n" +
+  "<span class=\"error\">#{testFailure.stacktrace}</span>\n\n\n"
+end
+
 def convert_new_lines(value)
   value.gsub(/\n/, "<br/>\n")
 end
--- app/models/test_failure_parser.rb
+++ app/models/test_failure_parser.rb
@@ -1,5 +1,5 @@
 class TestFailureParser
-  FIND_TEST_FAILURE_REGEX = /^\s+\d+.*\n\S*\s*\[([\s\S]*?)\]\:\n([\s\S]*?)\n\n/
+  FIND_TEST_FAILURE_REGEX = /^\s+\d+\)\s.*\:\n\S*\s*\[([\s\S]*?)\]\:\n([\s\S]*?)\n\n/
   def get_test_failures(log)
     testFailures = Array.new
   
