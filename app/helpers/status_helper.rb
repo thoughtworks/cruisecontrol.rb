da713e68eb84c4edfd53935f88671ecd0fde1658
Arthur Tam <arthur.tam@gmail.com>
2007-02-05 20:25:34 +0000

    r98: Unit tests for Project Status / Build Activity conversions for CCTray monitoring

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -1,11 +1,13 @@
-# TODO: Should we rename the CruiseControl.rb statuses to match those of
-# CruiseControl.NET or re-map them here?
+# Re-map our project statuses to match the project statuses recognized
+# by CCTray.Net
 def map_project_status_to_ccnet_project_status(value)
   return "Success" if value == :success.to_s or value == :building.to_s
   return "Unknown" if value == :never_built.to_s
   return "Failure"
 end
 
+# Re-map our build activities to match the build activities recognized
+# by CCTray.Net
 def map_build_activity_to_ccnet_activity(value)
   return "CheckingModifications" if value == :checking_for_modifications
   return "Building" if value == :building
