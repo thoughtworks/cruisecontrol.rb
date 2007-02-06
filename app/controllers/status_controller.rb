c340805c5a419c83038c52654a033b1d47504a26
Joe Poon <joe.poon@gmail.com>
2007-02-06 00:20:18 +0000

    r104: Added dashboard web url for CruiseControl.NET tray

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,6 +2,7 @@ class StatusController < ApplicationController
   # TODO: Investigate whether generation of XML 
   # of projects can be done in controller to allow testing
   def projects
+    @url = url_for(:controller => 'projects', :action => 'index')
     @projects = Projects.load_all
   end
 end
