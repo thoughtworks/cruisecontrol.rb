8f354e2d34c9e01b31d35b2d01b9e66397822166
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-02-06 00:56:03 +0000

    r105: Removing links to admin interface, breadcrumbs and some other cruft; dealt with a couple of UI enhancements in the process

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,44 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def add_breadcrumb(name, link)
-    if @breadcrumbs
-      @breadcrumbs << " > "
-    else
-      @breadcrumbs = ""
-    end
-    
-    @breadcrumbs << link_to(name, link)
-  end
-  
-  def dashboard_breadcrumb
-    add_breadcrumb 'Dashboard', '/'
-  end
-  
-  def server_settings_page_breadcrumb
-    dashboard_breadcrumb
-    add_breadcrumb 'Server Settings', :controller => 'admin', :action => 'server_settings'
-  end
-  
-  def email_settings_page_breadcrumb
-    server_settings_page_breadcrumb
-    add_breadcrumb 'E-Mail Settings', :controller => 'admin', :action => 'email_settings'
-  end
-  
-  def project_page_breadcrumb
-    dashboard_breadcrumb
-    add_breadcrumb @project.name, :controller => 'project', :action => 'show', :id => @project.url_name
-  end
-  
-  def project_settings_page_breadcrumb
-    project_page_breadcrumb
-    add_breadcrumb 'Settings', :controller => 'project', :action => 'settings', :id => @project.url_name
-  end
-  
-  def color_for_status(build)
-    build.successful? ? 'green' : 'red'
-  end
-
   def format_time(time, format = :iso)
     case format
     when :human
--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -1,20 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ServerTest < Test::Unit::TestCase
-  include FileSandbox
-  
-  def test_save_server
-    in_sandbox do |sandbox|
-      ActionMailer::Base.server_settings = {:foo => 'crap', :port => 13}
-      Server.new(sandbox.root).save
-      
-      ActionMailer::Base.server_settings = {:bar => 'cat'}
-      Server.new(sandbox.root).load
-    
-      assert_equal({:foo => 'crap', :port => 13}, ActionMailer::Base.server_settings)
-    end
-  end
-  
+
   def test_load_server_does_nothing_if_file_doesnt_exist
     Server.new('madeup_dir').load
   end
