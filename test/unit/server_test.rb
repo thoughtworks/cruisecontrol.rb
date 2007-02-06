44a956a5a811249dbcfb56ad7c0009de18a5252f
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-02-06 23:32:45 +0000

    r121: default environment for 'cruise start' is production. removing some more cruft

--- test/unit/server_test.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class ServerTest < Test::Unit::TestCase
-
-  def test_load_server_does_nothing_if_file_doesnt_exist
-    Server.new('madeup_dir').load
-  end
-end
