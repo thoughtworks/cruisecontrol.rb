a53a48bbc5c8049d872e11d0ae21b6fa27ac7da7
Jeremy Stell-Smith <jeremystellsmith@gmail.com>
2007-02-12 12:44:19 +0000

    r196: builders now start again on non-win 32

--- app/models/builder_starter.rb
+++ app/models/builder_starter.rb
@@ -19,8 +19,7 @@ class BuilderStarter
     if ruby_platform =~ /mswin32/
       Thread.new(project_name) { |my_project_name| system("cruise.cmd build #{project_name}") }
     else
-      fork
-      exec("#{RAILS_ROOT}/cruise build #{project_name}")
+      fork || exec("#{RAILS_ROOT}/cruise build #{project_name}")
     end
   end
   
