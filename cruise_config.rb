0c8e50a0335f7c812e9b25cd89e4926c2d355fdd
Jeremy Stell-Smith <jeremystellsmith@gmail.com>
2007-03-01 00:19:39 +0000

    r312: we should have our own cruise_config

---
+++ cruise_config.rb
@@ -0,0 +1,7 @@
+# this is the file that cruise control uses to configure its own cruise build at 
+# http://cruisecontrolrb.thoughtworks.com/
+#   simple, ain't it
+
+Project.configure do |project|
+  project.email_notifier.emails = ["cruisecontrol-developers@rubyforge.org"]
+end
