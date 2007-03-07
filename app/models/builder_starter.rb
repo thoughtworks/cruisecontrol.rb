7093d73ba7f0dcdec0452c62edd34d7994c9b79d
Jeremy Stell-Smith <jeremystellsmith@gmail.com>
2007-03-07 01:59:39 +0000

    r327: should configure using site_config before running the builders

--- app/models/builder_starter.rb
+++ app/models/builder_starter.rb
@@ -10,6 +10,7 @@ class BuilderStarter
   def self.start_builders
     if @@run_builders_at_startup
       Projects.load_all.each do |project|
+        puts "begin #{project.name}"
         begin_builder(project.name)
       end
     end
