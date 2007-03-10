101bb6c1852c7dcdaea34db59265d5f5d091a258
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-03-10 03:27:25 +0000

    r330: Turning HTTP sessions off. We dont need no frigging sessions!

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,8 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_ci_session_id'
+  session :off
 
   def render_not_found
     render :file => File.join(RAILS_ROOT, 'public/404.html'), :status => 404
