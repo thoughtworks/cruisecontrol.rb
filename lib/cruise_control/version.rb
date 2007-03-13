13125a71d658f668624167c486ffe21f4d0e4e87
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-03-13 15:46:52 +0000

    r350: Version 1.0.+

--- lib/cruise_control/version.rb
+++ lib/cruise_control/version.rb
@@ -3,7 +3,7 @@ module CruiseControl
     unless defined? MAJOR
       MAJOR = 1 
       MINOR = 0
-      MAINTENANCE = 0
+      MAINTENANCE = '+'
 
       STRING = [MAJOR, MINOR, MAINTENANCE].join('.')
     end
