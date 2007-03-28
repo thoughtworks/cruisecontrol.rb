f814faa1a7872c37954f71e8f50aa7525b49ebe0
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-03-28 05:10:14 +0000

    r415: Bumping the version number

--- lib/cruise_control/version.rb
+++ lib/cruise_control/version.rb
@@ -2,7 +2,7 @@ module CruiseControl
   module VERSION #:nodoc:
     unless defined? MAJOR
       MAJOR = 1 
-      MINOR = 0
+      MINOR = 1
       MAINTENANCE = 0
 
       STRING = [MAJOR, MINOR, MAINTENANCE].join('.')
