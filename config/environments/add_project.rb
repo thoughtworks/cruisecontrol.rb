40669f70ff07953cc8a28f7123d4b79cdf504da7
Jeremy Stell-Smith <jeremystellsmith@gmail.com>
2007-04-23 06:51:43 +0000

    r462: everyone should load site_config

--- config/environments/add_project.rb
+++ config/environments/add_project.rb
@@ -1,1 +1,2 @@
 # No special settings required
+require 'site_config' if File.exists?("#{RAILS_ROOT}/config/site_config.rb")
--- config/environments/builder.rb
+++ config/environments/builder.rb
@@ -1,3 +1,5 @@
 config.cache_classes = true
 config.log_path = CRUISE_OPTIONS[:log_file_name] || 'log/builder_WITHOUT_A_NAME.log'
 config.log_level = CRUISE_OPTIONS[:verbose] ? :debug : :info
+
+require 'site_config' if File.exists?("#{RAILS_ROOT}/config/site_config.rb")
