8d9b41ff67056a12a2daf6b0e506f743b5fb7798
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-05-27 07:07:33 +0000

    r489: Blow away cached pages when performing data migrations

--- lib/projects_migration.rb
+++ lib/projects_migration.rb
@@ -1,5 +1,6 @@
 class ProjectsMigration
   include CommandLine
+  include FileUtils
 
   def initialize(projects_directory = Configuration.projects_directory)
     @projects_directory = projects_directory
@@ -10,6 +11,7 @@ class ProjectsMigration
       script_version = script.to_i
       if script_version > current_data_version
         puts "Executing migration script #{script}. This may take some time..."
+        clear_cached_pages
         execute "ruby #{File.join(migrate_scripts_directory, script)} #{@projects_directory}"
         File.open(data_version_file, 'w') { |f| f.write(script_version) }
       end
@@ -30,6 +32,13 @@ class ProjectsMigration
 
   def data_version_file
     File.join(@projects_directory, 'data.version')
+  end
+
+  def clear_cached_pages
+    cached_assets_in_public = [ 'documentation', 'index.html']
+    cached_assets_in_public.each do |asset|
+      rm_rf File.join(RAILS_ROOT, 'public', asset)
+    end
   end
 
 end
