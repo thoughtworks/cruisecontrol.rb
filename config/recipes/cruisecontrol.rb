2a5851c447af98f4911cdcbbb39b497ce78116e1
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-06-25 19:25:16 +0000

    r512: change directory to [cruise] before doing anything else in ./cruise

--- config/recipes/cruisecontrol.rb
+++ config/recipes/cruisecontrol.rb
@@ -1,43 +1,33 @@
 Capistrano.configuration(:must_exist).load do
-  desc "deploy CC.rb"
-  task :deploy_cc_rb do
-    stop_cc_rb
-    
+
+  desc 'deploy CC.rb'
+  task :deploy_ccrb do
+    stop_ccrb
+
     update_code
     after_update_code
     symlink
-        
-    start_cc_rb
+
+    start_ccrb
   end
 
-  desc "stop CC.rb"
-  task :stop_cc_rb do
-    run "#{current_release}/daemon/cruise stop" rescue nil
+  desc 'stop CC.rb'
+  task :stop_ccrb do
+    sudo "sv force-stop ccrb_dashboard_1" rescue nil
+    sudo "sv force-stop ccrb_dashboard_2" rescue nil
   end
-  
+
   desc "start CC.rb"
-  task :start_cc_rb do
-    run "#{current_release}/daemon/cruise start"
+  task :start_ccrb do
+    sudo "sv start ccrb_dashboard_1" rescue nil
+    sudo "sv start ccrb_dashboard_2" rescue nil
   end
 
-  desc "Update symlink for projects directory."
+  desc 'Update symlink for projects directory'
   task :after_update_code do
-    run <<-CMD
-      rm -rf #{release_path}/projects &&
-      mkdir -p #{shared_path}/projects &&
-      ln -nfs #{shared_path}/projects #{release_path}/projects &&
-      rm -rf #{release_path}/daemon &&
-      mkdir -p #{shared_path}/daemon &&
-      ln -nfs #{shared_path}/daemon #{release_path}/daemon
-    CMD
+    sudo "rm -rf #{release_path}/projects"
+    sudo "mkdir -p #{shared_path}/projects"
+    sudo "ln -nfs #{shared_path}/projects #{release_path}/projects"
   end
 
-  desc "Add a project to cruise control."
-  task :add_project do
-    unless ENV['NAME'] && ENV['URL'] 
-      raise ArgumentError, "***** You must specify the NAME and URL parameters to add a project. *****" 
-    end   
-    run "#{current_release}/cruise add #{ENV['NAME']} -u #{ENV['URL']}"
-  end
-    
 end
