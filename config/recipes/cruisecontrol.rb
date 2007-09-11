8a4ab304c54921636582276a6a94c5ff826ff013
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2007-09-11 21:36:18 +0000

    r534: don't really need to touch cc_context_webapp at startup

--- config/recipes/cruisecontrol.rb
+++ config/recipes/cruisecontrol.rb
@@ -1,37 +1,39 @@
-Capistrano.configuration(:must_exist).load do
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :deploy do
 
-  desc 'deploy CC.rb'
-  task :deploy_ccrb do
-    stop_ccrb
+    desc 'deploy CC.rb'
+    task :ccrb do
+      stop_ccrb
 
-    update_code
-    after_update_code
-    symlink
+      update_code
+      after_update_code
+      symlink
 
-    start_ccrb
+      start_ccrb
+    end
+
+    desc 'stop CC.rb'
+    task :stop_ccrb do
+      sudo "sv force-stop ccrb_dashboard_1" rescue nil
+      sudo "sv force-stop ccrb_dashboard_2" rescue nil
+    end
+
+    desc "start CC.rb"
+    task :start_ccrb do
+      sudo "sv start ccrb_dashboard_1" rescue nil
+      sudo "sv start ccrb_dashboard_2" rescue nil
+    end
+
+    desc 'Update symlink for projects directory'
+    task :after_update_code do
+      sudo "rm -rf #{release_path}/projects"
+      sudo "mkdir -p #{shared_path}/projects"
+      sudo "chown deployer #{shared_path}/projects"
+      sudo "chgrp rails #{shared_path}/projects"
+      sudo "ln -nfs #{shared_path}/projects #{release_path}/projects"
+      sudo "touch #{shared_path}/site_config.rb"
+      sudo "ln -nfs #{shared_path}/site_config.rb #{release_path}/config/site_config.rb"
+    end
+
   end
-
-  desc 'stop CC.rb'
-  task :stop_ccrb do
-    sudo "sv force-stop ccrb_dashboard_1" rescue nil
-    sudo "sv force-stop ccrb_dashboard_2" rescue nil
-  end
-
-  desc "start CC.rb"
-  task :start_ccrb do
-    sudo "sv start ccrb_dashboard_1" rescue nil
-    sudo "sv start ccrb_dashboard_2" rescue nil
-  end
-
-  desc 'Update symlink for projects directory'
-  task :after_update_code do
-    sudo "rm -rf #{release_path}/projects"
-    sudo "mkdir -p #{shared_path}/projects"
-    sudo "chown deployer #{shared_path}/projects"
-    sudo "chgrp rails #{shared_path}/projects"
-    sudo "ln -nfs #{shared_path}/projects #{release_path}/projects"
-    sudo "touch #{shared_path}/site_config.rb"
-    sudo "ln -nfs #{shared_path}/site_config.rb #{release_path}/config/site_config.rb"
-  end
-
 end
