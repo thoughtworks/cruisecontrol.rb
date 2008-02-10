76f1c082c2e17afb02aada59d99be233fcedb46e
Jeremy Stell-Smith <jeremystellsmith@gmail.com>
2008-02-10 06:29:12 +0000

    r598: logging for site_config not getting loaded

--- config/dashboard_initialize.rb
+++ config/dashboard_initialize.rb
@@ -2,7 +2,13 @@
 # Local configuration, for example, details of the SMTP server for email notification, should be 
 # written in ./config/site_config.rb. See ./config/site_config.rb_example for an example of what this file may 
 # look like.
-require CRUISE_DATA_ROOT + '/site_config' if File.exists?(CRUISE_DATA_ROOT + "/site_config.rb")
+site_config = CRUISE_DATA_ROOT + "/site_config.rb"
+if File.exists?(site_config)
+  puts "loading #{site_config}"
+  require site_config
+else
+  puts "didn't find #{site_config}, create one to customize cruisecontrol.rb"
+end
 
 site_css = CRUISE_DATA_ROOT + "/site.css"
 if File.exists?(site_css)
