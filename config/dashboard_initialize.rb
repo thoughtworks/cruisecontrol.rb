960bf7c31b719b7959d5c6d24885d1287454820e
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2008-05-07 11:49:37 -0600

    some tweaks for the earlier checkin

--- config/dashboard_initialize.rb
+++ config/dashboard_initialize.rb
@@ -1,4 +1,4 @@
-site_css = File.join(CRUISE_DATA_ROOT, "/site.css")
+site_css = File.join(CRUISE_DATA_ROOT, "site.css")
 if File.exists?(site_css)
   copy_of_site_css = File.join(RAILS_ROOT, 'public', 'stylesheets', 'site.css')
   begin
@@ -7,6 +7,6 @@ if File.exists?(site_css)
       f << File.read(site_css)
     end
   rescue
-    CruiseControl::Log.info("Could not copy contents of #{site_css} to #{RAILS_ROOT}/public/stylesheets/site.css")
+    CruiseControl::Log.warn("Could not copy contents of #{site_css} to #{copy_of_site_css}")
   end
 end
