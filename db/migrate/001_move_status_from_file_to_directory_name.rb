0e7f05c6ef27721fd6d6fc4ee10836d502a3b6d4
Alexey Verkhovsky <alexey.verkhovsky@gmail.com>
2008-05-28 18:27:06 -0600

    fixed a bug in an old migration [http://cruisecontrolrb.lighthouseapp.com/projects/9150/tickets/6]

--- db/migrate/001_move_status_from_file_to_directory_name.rb
+++ db/migrate/001_move_status_from_file_to_directory_name.rb
@@ -5,6 +5,12 @@ Dir["projects/*/build-*"].reject{|path| path =~ /build-.*-.*in.*s/}.each do |dir
   status_file = Dir["#{dir}/build_status.*"].first
   next unless status_file
   status_info = File.basename(status_file).sub('build_status.', '')
+
+  new_dirname = "#{dir}-#{status_info}"
+  time_stamp = File.mtime(dir)
+
   rm status_file
-  mv dir, "#{dir}-#{status_info}"
+  mv dir, new_dirname
+
+  File.utime(time_stamp, time_stamp, new_dirname)
 end
