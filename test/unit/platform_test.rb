0d23184f16e481c6b32af3a3d9367fae9e7124d1
Michael Schubert <michael@schubert.cx>
2008-05-31 21:13:38 +0000

    Applied patch to prevent zombie processes. See ruby docs on #detach for explaination. From ThoughtWorker Kurtis Seebaldt [http://cruisecontrolrb.lighthouseapp.com/projects/9150/tickets/192]

---
+++ test/unit/platform_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PlatformTest < Test::Unit::TestCase
+  include FileSandbox
+  
+  def test_create_child_process_detaches_to_avoid_zombie_processes
+    Platform.expects(:fork).returns(123)
+    Process.expects(:detach).with(123)
+    
+    Platform.create_child_process("project", "command")
+  end
+  
+end
