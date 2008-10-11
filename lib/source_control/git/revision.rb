25575bcfe78af981680692118ae14cb5548d6c88
jeremylightsmith <jeremy.lightsmith@gmail.com>
2008-10-11 10:09:56 -0700

    we already have a temporary solution for to_s

--- lib/source_control/git/revision.rb
+++ lib/source_control/git/revision.rb
@@ -7,12 +7,6 @@ module SourceControl
         @number, @author, @time = number, author, time
       end
 
-      def to_s
-        <<-EOL
-Revision #{number} committed by #{author} on #{time.strftime('%Y-%m-%d %H:%M:%S') if time}
-        EOL
-      end
-
       def ==(other)
         other.is_a?(Git::Revision) && number == other.number
       end
