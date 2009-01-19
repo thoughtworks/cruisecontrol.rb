3e275bb61865d8c2d6a94211ec3fb56709dbac95
jeremylightsmith <jeremy.lightsmith@gmail.com>
2009-01-19 12:58:22 -0800

    taking ffmike's changes

--- lib/source_control/git/log_parser.rb
+++ lib/source_control/git/log_parser.rb
@@ -12,7 +12,7 @@ module SourceControl
           case line
           when /^commit /
             revisions << revision = Revision.new
-            revision.number = line.split[1][0..4]
+            revision.number = line.split[1][0..5]
             
           when /^author /
             revision.author, revision.time = read_author_and_time(line)
