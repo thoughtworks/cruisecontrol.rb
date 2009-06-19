d7e4dbd9745a6e34a4629b5258d24bac767e7751
jeremylightsmith <jeremy.lightsmith@gmail.com>
2009-06-18 23:37:57 -0700

    removing rubyforge urls

--- script/package_zip.bat
+++ script/package_zip.bat
@@ -2,7 +2,7 @@
 @echo on
 rm *.zip
 rm -rf \tmp\cruise
-svn co svn://rubyforge.org/var/svn/cruisecontrolrb/trunk /tmp/cruise
+git clone git://github.com/thoughtworks/cruisecontrol.rb.git /tmp/cruise
 
 pushd .
 cd \tmp\cruise
