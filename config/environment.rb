d70773b4b5dd6c33501227c42a6530040fd5ce84
Chad Woolley <thewoolleyman@gmail.com>
2009-08-30 03:38:09 -0700

    do not use rcov under ruby 1.8.7 or 1.9

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
   config.gem "xml-simple", :lib => "xmlsimple"
   config.gem "mocha"
-  config.gem "rcov"
+  config.gem "rcov" if RUBY_VERSION != '1.8.7' && RUBY_VERSION !~ /^1.9/
   
   def find_home
     looks_like_windows = (Config::CONFIG["target_os"] =~ /32/)
