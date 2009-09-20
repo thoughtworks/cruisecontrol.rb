883a71537579b69fc010112222911ff664aa1edb
Matt Duncan <mrduncan@gmail.com>
2009-09-20 10:38:09 -0400

    Fixing subversion info parser bug on ruby 1.9

--- lib/source_control/subversion/info_parser.rb
+++ lib/source_control/subversion/info_parser.rb
@@ -3,7 +3,7 @@ module SourceControl
 
     class InfoParser
       def parse(xml)
-        info = XmlSimple.xml_in(xml.to_s, 'ForceArray' => false)['entry']
+        info = XmlSimple.xml_in(xml.join, 'ForceArray' => false)['entry']
         Subversion::Info.new(info['revision'].to_i, info['commit']['revision'].to_i, info['commit']['author'])
       end
     end
