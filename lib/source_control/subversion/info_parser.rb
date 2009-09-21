36e86c8ba418e2d36376485c94e8cf1fc18e7df3
Matt Duncan <mrduncan@gmail.com>
2009-09-20 22:12:44 -0400

    Fixing the rest of the failing tests for ruby 1.8 and 1.9

--- lib/source_control/subversion/info_parser.rb
+++ lib/source_control/subversion/info_parser.rb
@@ -3,7 +3,8 @@ module SourceControl
 
     class InfoParser
       def parse(xml)
-        info = XmlSimple.xml_in(xml.join, 'ForceArray' => false)['entry']
+        xml = xml.join if xml.is_a? Array
+        info = XmlSimple.xml_in(xml.to_s, 'ForceArray' => false)['entry']
         Subversion::Info.new(info['revision'].to_i, info['commit']['revision'].to_i, info['commit']['author'])
       end
     end
