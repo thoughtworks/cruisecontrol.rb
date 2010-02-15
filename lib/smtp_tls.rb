34fc3f8d1250570ec6cb8e06c94a60f76d8c4770
Brian Guthrie <btguthrie@gmail.com>
2010-02-15 14:19:10 -0600

    Moved TLS fix over to a Ruby version check rather than an arity check. Both are fragile but this is more explicit.

--- lib/smtp_tls.rb
+++ lib/smtp_tls.rb
@@ -6,14 +6,10 @@ Net::SMTP.class_eval do
     raise IOError, 'SMTP session already started' if @started
 
     if user or secret
-      arity = instance_method(:check_auth_args).arity
-      
-      if arity == 2
-        #Ruby 1.8.7 and above -- check_auth_args takes two args, check_auth_method is separate
+      if RUBY_VERSION < "1.8.7"
         check_auth_method(authtype || DEFAULT_AUTH_TYPE)
         check_auth_args user, secret
       else
-        #Ruby 1.8.6 and below -- check_auth_args takes three args
         check_auth_args user, secret, authtype
       end
     end
