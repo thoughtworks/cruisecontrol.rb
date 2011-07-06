d3ffb718eb5cb1e0f9c9fce084cfb8840480e828
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2011-07-06 08:12:10 -0700

    Remove the RAILS_RELATIVE_URL_ROOT environment since that's used by rails to determine the context path causing the build to fail.

--- app/models/build.rb
+++ app/models/build.rb
@@ -202,11 +202,13 @@ EOF
   def in_clean_environment_on_local_copy(&block)
     old_rails_env = ENV['RAILS_ENV']
     old_bundle_gemfile = ENV['BUNDLE_GEMFILE']
+    old_rails_context_path = ENV['RAILS_RELATIVE_URL_ROOT']
 
     Bundler.with_clean_env do
       begin
         ENV['RAILS_ENV'] = nil
         ENV['BUNDLE_GEMFILE'] = nil
+        ENV['RAILS_RELATIVE_URL_ROOT'] = nil
 
         # set OS variable CC_BUILD_ARTIFACTS so that custom build tasks know where to redirect their products
         ENV['CC_BUILD_ARTIFACTS'] = self.artifacts_directory
@@ -223,6 +225,7 @@ EOF
       ensure
         ENV['RAILS_ENV'] = old_rails_env
         ENV['BUNDLE_GEMFILE'] = old_bundle_gemfile
+        ENV['RAILS_RELATIVE_URL_ROOT'] = old_rails_context_path
       end
     end
   end
