dc26434e655409c203c5c7c2e5d5d0f063d15573
Mario Zaizar <mariozaizar@gmail.com>
2012-09-10 15:37:09 -0500

    Fixing the build script tab from Project's details.

--- app/models/build.rb
+++ app/models/build.rb
@@ -126,8 +126,8 @@ EOF
   end
 
   def build_script
-    @build_script ||= contents_for_display(work('script/build'))
-    @build_script ||= contents_for_display(work('build.sh'))
+    @build_script = contents_for_display(work('script/build')) if @build_script.blank?
+    @build_script = contents_for_display(work('build.sh')) if @build_script.blank?
   end
 
   def error
